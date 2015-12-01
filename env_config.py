aca7732ef9a15c4b974bb78d18a3194a6ad59274
Marco Caberletti <marco.caberletti@gmail.com>
2015-12-01 16:43:46 +0100

    Add configuration tests.

---
+++ env_config.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+# Variables set to run the scripts on your system
+
+# Locations for storing needed files
+TMP_DIR='/tmp'
+
+# Home Directories
+T_PAP_HOME='/usr/share/argus/pap'
+T_PDP_HOME='/usr/share/argus/pdp'
+T_PEP_HOME='/usr/share/argus/pepd'
+
+# Configuration scripts and locations
+T_PDP_CONF=T_PDP_HOME + '/conf'
+T_PEP_CONF=T_PEP_HOME + '/conf'
+T_PAP_CONF=T_PAP_HOME + '/conf'
+T_PDP_INI='pdp.ini'
+T_PEP_INI='pepd.ini'
+T_PAP_ADMIN_INI='pap-admin.properties'
+T_PAP_AUTH_INI='pap_authorization.ini'
+T_PAP_CONF_INI='pap_configuration.ini'
+
+T_PAP_PORT='8150'
+T_PAP_ADMIN_PORT='8151'
+T_PDP_PORT='8152'
+T_PDP_ADMIN_PORT='8153'
+T_PEP_PORT='8154'
+T_PEP_ADMIN_PORT='8154'
+
+# Init-scripts
+T_PAP_CTRL='papctl'
+T_PDP_CTRL='pdpctl'
+T_PEP_CTRL='pepdctl'
+
+# CLI's
+PEPCLI='/usr/bin/pepcli'
+PAP_ADMIN='/usr/bin/pap-admin'
+
+# Grid specific files
+GRIDDIR='/mnt/grid-security'
+GRID_MAPFILE='grid-mapfile'
+GROUPMAPFILE='groupmapfile'
+GRIDMAPDIR='gridmapdir'
+
+# Test specific configurations
+USERCERT='~/user_cert/test0.cert.pem'
+USERKEY='~/user_cert/test0.key.pem'
+USERKEYRSA='~/user_cert/test0.newkey.pem'
+# in case of empty passphrase set this to /dev/null
+USERPWD_FILE='~/user_cert/password'
+
+VO="test.vo"
+VO_PRIMARY_GROUP="/test.vo"
+VO_SECONDARY_GROUP="/test.vo"
+VOMSES_STRING='"test.vo" "vgrid02.cnaf.infn.it" "15004" "/C=IT/O=INFN/OU=Host/CN=vgrid02.cnaf.infn.it" "test.vo"'
+

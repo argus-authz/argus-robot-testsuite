88943d40a16f2857fee441107142b9b350b531df
Federica Agostini <federica.agostini@cnaf.infn.it>
2021-06-25 10:55:46 +0200

    WIP fix build image

--- docker/files/manifest.pp
+++ docker/files/manifest.pp
@@ -1,42 +1,63 @@
+include epel
+include umd4
+include testvos
+include testca
 
-$packages = ['ca_TERENA-eScience-SSL-CA-3', 'curl', 'voms-clients-java', 'myproxy', 'voms-test-ca']
+$packages = [
+  'openldap-clients',
+  'globus-gass-copy-progs',
+  'gfal2-util',
+  'gfal2-all',
+  'dcache-srmclient',
+  'davix',
+  'voms-clients-java',
+  'voms-test-ca'
+]
 
-$voms_str = '/DC=org/DC=terena/DC=tcs/C=IT/ST=Lazio/L=Frascati/O=Istituto Nazionale di Fisica Nucleare/CN=vgrid02.cnaf.infn.it
-/C=NL/ST=Noord-Holland/L=Amsterdam/O=TERENA/CN=TERENA eScience SSL CA 3'
+include 'mwdevel_argus::clients'
 
-class { 'mwdevel_infn_ca': } ->
-class { 'mwdevel_test_ca': } ->
-class { 'mwdevel_robot_framework': } ->
-class { 'mwdevel_argus::clients': } ->
-package { $packages: ensure => latest, } ->
+package { $packages:
+  ensure => 'latest',
+}
+
+package { 'voms-clients':
+  ensure => 'installed',
+  source => 'https://ci.cloud.cnaf.infn.it/view/voms/job/pkg.voms/job/release_sep_19/lastSuccessfulBuild/artifact/repo/centos6/voms-clients3-3.3.1-0.el6.centos.noarch.rpm',
+}
+
+include python
+
+package { 'robotframework':
+  ensure   => installed,
+  require  => Package['pip'],
+  provider => 'pip',
+}
+package { 'robotframework-httplibrary':
+  ensure   => installed,
+  require  => [Package['pip'], Package['robotframework']],
+  provider => 'pip',
+}
+
+class { 'java' :
+  package => 'java-1.8.0-openjdk-devel',
+}
+
 user { 'tester':
   ensure     => present,
-  name       => 'tester',
-  managehome => true
-} ->
-file {
-  '/etc/vomses':
-    ensure => directory;
-
-  '/etc/grid-security/vomsdir':
-    ensure => directory;
-
-  '/etc/grid-security/vomsdir/test.vo':
-    ensure => directory;
-
-  '/etc/vomses/test.vo-vgrid02.cnaf.infn.it':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => '"test.vo" "vgrid02.cnaf.infn.it" "15000" "/DC=org/DC=terena/DC=tcs/C=IT/ST=Lazio/L=Frascati/O=Istituto Nazionale di Fisica Nucleare/CN=vgrid02.cnaf.infn.it" "test.vo" "24"',
-    require => File['/etc/vomses'];
-
-  '/etc/grid-security/vomsdir/test.vo/vgrid02.cnaf.infn.it.lsc':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => $voms_str,
-    require => File['/etc/grid-security/vomsdir/test.vo'];
+  name       => $title,
+  password   => Sensitive('password'),
+  managehome => true,
+  groups     => ['wheel'],
 }
+
+Class['epel']
+-> Class['umd4']
+-> Class['python']
+-> Class['java']
+-> Class['testvos']
+-> Class['testca']
+-> Class['storm::repo']
+-> Package[$packages]
+-> Package['voms-clients']
+-> Package['robotframework']
+-> User['tester']

5d2f58ca64029a12f96e6aea48dce8c840af2b91
Federica Agostini <federica.agostini@cnaf.infn.it>
2021-07-01 12:51:38 +0200

    Added tag to docker image

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -2,4 +2,11 @@
 
 set -xe
 
-docker build --no-cache -t italiangrid/argus-testsuite .
+if [ -z ${BRANCH_NAME+x} ]; then
+  echo "BRANCH_NAME is unset"
+  BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+else
+  echo "BRANCH_NAME is set to '$BRANCH_NAME'"
+fi
+
+docker build --no-cache -t italiangrid/argus-testsuite:${BRANCH_NAME} .

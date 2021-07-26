fc051a3ea230ab7346bff582f54ef467e293b661
Federica Agostini <federica.agostini@cnaf.infn.it>
2021-07-26 15:44:29 +0200

    Small fixes

--- compose/assets/scripts/setup-and-run-argus.sh
+++ compose/assets/scripts/setup-and-run-argus.sh
@@ -2,10 +2,17 @@
 
 set -xe
 
-wget https://ci.cloud.cnaf.infn.it/view/repos/job/repo_test_ca/lastSuccessfulBuild/artifact/test-ca.repo -O /etc/yum.repos.d/test-ca.repo
-yum reinstall -y igi-test-ca
+#yum downgrade -y java-1.8.0-openjdk java-1.8.0-openjdk-headless 
 
-wget http://repository.egi.eu/sw/production/cas/1/current/repo-files/EGI-trustanchors.repo -O /etc/yum.repos.d/EGI-trustanchors.repo
+TEST_CA_REPO_URL=${TEST_CA_REPO_URL:-https://ci.cloud.cnaf.infn.it/view/repos/job/repo_test_ca/lastSuccessfulBuild/artifact/test-ca.repo}
+EGI_TRUSTANCHORS_REPO_URL=${EGI_TRUSTANCHORS_REPO_URL:-http://repository.egi.eu/sw/production/cas/1/current/repo-files/EGI-trustanchors.repo}
+
+VGRID02_LSC=${VGRID02_LSC:-https://raw.githubusercontent.com/italiangrid/voms-testsuite/master/compose/assets/vomsdir/test.vo/vgrid02.cnaf.infn.it.lsc}
+TESTVO_VOMSES=${TESTVO_VOMSES:-https://raw.githubusercontent.com/italiangrid/voms-testsuite/master/compose/assets/vomses/test.vo.vomses}
+
+wget ${TEST_CA_REPO_URL} -O /etc/yum.repos.d/test-ca.repo
+wget ${EGI_TRUSTANCHORS_REPO_URL} -O /etc/yum.repos.d/EGI-trustanchors.repo
+
 yum install -y ca-policy-egi-core
 
 # Setup host certificate
@@ -22,8 +29,8 @@ sed -i -e "s#argus-pap.example.org#${HOSTNAME}#g" /etc/argus/pdp/pdp.ini
 sed -i -e "s#argus.example.org#${HOSTNAME}#g" /etc/argus/pepd/pepd.ini
 
 mkdir -p /etc/grid-security/vomsdir/test.vo/ /etc/vomses/
-wget https://raw.githubusercontent.com/italiangrid/voms-testsuite/master/compose/assets/vomsdir/test.vo/vgrid02.cnaf.infn.it.lsc -O /etc/grid-security/vomsdir/test.vo/vgrid02.cnaf.infn.it.lsc
-wget https://raw.githubusercontent.com/italiangrid/voms-testsuite/master/compose/assets/vomses/test.vo.vomses -O /etc/vomses/test.vo.vomses
+wget ${VGRID02_LSC} -O /etc/grid-security/vomsdir/test.vo/vgrid02.cnaf.infn.it.lsc
+wget ${TESTVO_VOMSES} -O /etc/vomses/test.vo.vomses
 touch /etc/grid-security/voms-grid-mapfile
 
 touch /etc/grid-security/grid-mapfile
